8ab623bd15f52a45f4dec56973a117c26a3f22d2
Yacine <yacine.akli97@gmail.com>
2025-01-04 22:48:38 +0100

    Add some json helpers and a Makefile

--- broker-service/broker-service.dockerfile
+++ broker-service/broker-service.dockerfile
@@ -1,21 +1,7 @@
-# Base Go image
-FROM golang:1.23.4-alpine as builder
-
-RUN mkdir -p /app
-
-COPY . /app
-
-WORKDIR /app
-
-RUN CGO_ENABLED=0 go build -o brokerApp ./cmd/api
-
-RUN chmod +x brokerApp
-
-# Tiny docker image
 FROM alpine:latest
 
 RUN mkdir -p /app
 
-COPY --from=builder /app/brokerApp /app
+COPY brokerApp /app
 
 CMD ["/app/brokerApp"]
--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool        `json:"error"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-}
 
 func (app *Application) Broker(w http.ResponseWriter, r *http.Request) {
 	response := jsonResponse{
@@ -17,13 +10,5 @@ func (app *Application) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Broker endpoint reached successfully",
 	}
 
-	output, err := json.MarshalIndent(response, "", "\t")
-	if err != nil {
-		http.Error(w, "Unable to process the request", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	_, _ = w.Write(output)
+	_ = app.writeJSON(w, http.StatusOK, response)
 }
