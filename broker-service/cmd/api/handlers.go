857985cba5537a933a58c305ad664bcd0a99acff
Yacine <yacine.akli97@gmail.com>
2025-01-06 03:59:26 +0100

    Add btn to test email

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -154,7 +154,7 @@ func (app *Application) sendMail(w http.ResponseWriter, msg MailPayload) {
 	jsonData, _ := json.MarshalIndent(msg, "", "\t")
 
 	// call the mail service
-	mailServiceURL := "http://mail-service/send"
+	mailServiceURL := "http://mailer-service/send"
 
 	// post to mail service
 	request, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))
